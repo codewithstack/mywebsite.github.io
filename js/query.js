bcaa1e742fada838f8baae35e2998d3cf422ba71
Yash Sharma <yashsharmadelhi01@gmail.com>
2021-03-27 23:03:28 +0530

    update

--- js/query.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$(window).scroll(function () {
-      $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-	}).on('click', '.dropdown-menu', function (e) {
-	e.stopPropagation();
-  });
-}); 
